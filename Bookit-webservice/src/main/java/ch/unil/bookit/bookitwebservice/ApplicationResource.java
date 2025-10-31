a36907e97ff05a09a1a5a445e580b54ef8eaf0af
AnaPao1998 <anapao.montero@gmail.com>
2025-10-31 17:10:26 +0100

    Modifying structure to be a multi-module project. Added CRUD methods to Guests.

--- Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/ApplicationResource.java
+++ Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/ApplicationResource.java
@@ -1,4 +1,56 @@
 package ch.unil.bookit.bookitwebservice;
+import ch.unil.bookit.domain.Guest;
+import ch.unil.bookit.domain.Hotel;
+import ch.unil.bookit.domain.User;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@ApplicationScoped
 public class ApplicationResource {
-}
+    private Map<UUID, Guest> guests;
+    private Map<UUID, Hotel> hotels;
+
+    @PostConstruct
+    public void init() {
+        guests = new HashMap<>();
+        hotels = new HashMap<>();
+
+        //dummy test
+        UUID guestId = UUID.randomUUID();
+        Guest guest1 = new Guest(guestId, "guest1@bookit.com", "pass123", "Ana", "Montero");
+        guest1.deposit(500);
+        guests.put(guestId, guest1);
+    }
+
+        //create
+        public Guest createGuest(Guest guest){
+            guest.setuuid(UUID.randomUUID());
+            guests.put(guest.getUUID(), guest);
+            return guest;
+        }
+
+        //read
+        public Guest getGuest(UUID id){
+            return guests.get(id);
+        }
+
+        //update
+        public Guest updateGuest(UUID id, Guest updatedGuest) {
+            if(guests.containsKey(id)){
+                updatedGuest.setuuid(id);
+                guests.put(id, updatedGuest);
+                return updatedGuest;
+            }
+            return null; //if guest not found return null
+        }
+        //delete
+        public boolean deleteGuest(UUID id){
+            return guests.remove(id) != null;
+        }
+    }
+
+
--- Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/GuestResource.java
+++ Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/GuestResource.java
@@ -1,4 +1,67 @@
 package ch.unil.bookit.bookitwebservice;
 
+import ch.unil.bookit.domain.Guest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.UUID;
+
+@Path("/guests")
+
 public class GuestResource {
+    @Inject
+    private ApplicationResource applicationResource;
+
+    //create
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createGuest(Guest guest) {
+        if (guest == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        Guest newGuest = applicationResource.createGuest(guest);
+        return Response.status(Response.Status.CREATED).entity(newGuest).build();
+    }
+
+    //read
+    @GET
+    @Path("/{guestId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getGuests(@PathParam("guestId")UUID guestId) {
+        Guest guest = applicationResource.getGuest(guestId);
+        if (guest != null) {
+            return Response.ok(guest).build(); //200 ok
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build(); //404
+        }
+
+    }
+
+    //update
+    @PUT
+    @Path("/{guestId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateGuest(@PathParam("guestId")UUID guestId, Guest guest) {
+        Guest result = applicationResource.updateGuest(guestId, guest);
+        if (guest != null) {
+            return Response.ok(guest).build();
+        } else  {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+
+    //delete
+    @DELETE
+    @Path("/{guestId}")
+    public Response deleteGuest(@PathParam("guestId")UUID guestId) {
+        if (applicationResource.deleteGuest(guestId)) {
+            return Response.noContent().build(); //204
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
 }
