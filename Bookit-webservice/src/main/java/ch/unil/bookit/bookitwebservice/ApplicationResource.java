2680922e371d7684a6ba440cf3735f314dc03d0b
bogdanicduska <bogdanic.duska@gmail.com>
2025-11-02 00:22:32 +0100

    adding extra code

--- Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/ApplicationResource.java
+++ Bookit-webservice/src/main/java/ch/unil/bookit/bookitwebservice/ApplicationResource.java
@@ -18,39 +18,46 @@ public class ApplicationResource {
     public void init() {
         guests = new HashMap<>();
         hotels = new HashMap<>();
+        populateApplicationState();
+    }
 
+    //create
+    public Guest createGuest(Guest guest){
+        guest.setuuid(UUID.randomUUID());
+        guests.put(guest.getUUID(), guest);
+        return guest;
+    }
+
+    public Map<UUID, Guest> getAllGuests() {
+        return guests;
+    }
+    //read
+    public Guest getGuest(UUID id){
+        return guests.get(id);
+    }
+
+    //update
+    public Guest updateGuest(UUID id, Guest updatedGuest) {
+        if(guests.containsKey(id)){
+            updatedGuest.setuuid(id);
+            guests.put(id, updatedGuest);
+            return updatedGuest;
+        }
+        return null; //if guest not found return null
+    }
+    //delete
+    public boolean deleteGuest(UUID id){
+        return guests.remove(id) != null;
+    }
+
+    private void populateApplicationState(){
         //dummy test
         UUID guestId = UUID.randomUUID();
+        UUID guestId2 = UUID.randomUUID();
         Guest guest1 = new Guest(guestId, "guest1@bookit.com", "pass123", "Ana", "Montero");
         guest1.deposit(500);
         guests.put(guestId, guest1);
+        Guest guest2 = new Guest(guestId2, "guest2@bookit.com", "pass123", "Duska", "Bogdanic");
+        guests.put(guestId2, guest2);
     }
-
-        //create
-        public Guest createGuest(Guest guest){
-            guest.setuuid(UUID.randomUUID());
-            guests.put(guest.getUUID(), guest);
-            return guest;
-        }
-
-        //read
-        public Guest getGuest(UUID id){
-            return guests.get(id);
-        }
-
-        //update
-        public Guest updateGuest(UUID id, Guest updatedGuest) {
-            if(guests.containsKey(id)){
-                updatedGuest.setuuid(id);
-                guests.put(id, updatedGuest);
-                return updatedGuest;
-            }
-            return null; //if guest not found return null
-        }
-        //delete
-        public boolean deleteGuest(UUID id){
-            return guests.remove(id) != null;
-        }
-    }
-
-
+}
