010e20c7ab6b21f5e5ba70b496ef2f7a7571d842
bogdanicduska <bogdanic.duska@gmail.com>
2025-11-21 18:37:45 +0100

    adding UserData and fix Login

--- bookit-webapp/src/main/java/ch/unil/bookit/bookitwebapp/ui/UserData.java
+++ bookit-webapp/src/main/java/ch/unil/bookit/bookitwebapp/ui/UserData.java
@@ -1,4 +1,17 @@
 package ch.unil.bookit.bookitwebapp.ui;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Named;
+
+@Named
+@ApplicationScoped
 public class UserData {
+
+    public String getManagerRole() {
+        return "manager";
+    }
+
+    public String getGuestRole() {
+        return "guest";
+    }
 }
