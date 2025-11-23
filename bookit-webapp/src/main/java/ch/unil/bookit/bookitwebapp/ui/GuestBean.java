67387f45854ec2eef69ef84e710b4c9e91f16e59
MarionZimmerlin <zimmerlinmarion@gmail.com>
2025-11-23 19:07:28 +0100

    GuestHome.xhtml GuestProfile.xhtml

--- bookit-webapp/src/main/java/ch/unil/bookit/bookitwebapp/ui/GuestBean.java
+++ bookit-webapp/src/main/java/ch/unil/bookit/bookitwebapp/ui/GuestBean.java
@@ -133,8 +133,4 @@ public class GuestBean extends Guest implements Serializable {
         boolean passwordChanged = !guest.getPassword().equals(this.getPassword());
         changed = firstNameChanged || lastNameChanged || emailChanged || passwordChanged;
     }
-
-    public boolean isChanged() {
-        return changed;
-    }
 }
